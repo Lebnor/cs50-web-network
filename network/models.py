ad8d699bca0f7f8eaaa060d88d02006f6c7fbee0
Liel Ben-Or <liel4349@gmail.com>
2021-04-16 17:35:07 +0300

    remove natural key from User model

--- network/models.py
+++ network/models.py
@@ -3,20 +3,8 @@ from django.db import models
 
 
 
-class UserManager(models.Manager):
-    def get_by_natural_key(self, first_name, last_name):
-        return self.get(first_name=first_name, last_name=last_name)
-
 class User(AbstractUser):
     
-    def natural_key(self):
-        return (self.first_name, self.last_name)
-
-    objects = UserManager()
-
-    class Meta:
-        unique_together = [['first_name', 'last_name']]
-
     def __str__(self):
         return f"{self.username}"
 
@@ -35,7 +23,7 @@ class Post(models.Model):
             "timestamp": self.timestamp
         }
     def __str__(self):
-        return f"{self.text}"
+        return serialize(self)
 
 class Comment(models.Model):
     text = models.TextField()
