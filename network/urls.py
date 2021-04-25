0b4e58378b6965a4c5ec34ae7418b83401d7bfdb
Liel Ben-Or <liel4349@gmail.com>
2021-04-25 23:41:44 +0300

    urls.py

--- network/urls.py
+++ network/urls.py
@@ -1,12 +1,12 @@
 
-from django.urls import path
+from django.urls import path, re_path
 from django.contrib import admin
 from django.views.generic import TemplateView 
 from . import views
 
 urlpatterns = [
     # path("", views.index, name="index"),
-    path('*', TemplateView.as_view(template_name='index.html')),
+    re_path('.*', TemplateView.as_view(template_name='index.html')),
     path('admin', admin.site.urls),
     path("login", views.login_view, name="login"),
     path("logout", views.logout_view, name="logout"),
